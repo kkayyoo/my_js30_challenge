14fef2dc847d2358f8ba4691070635a9a6a74238
kkayyoo <yokoyau159@gmail.com>
2017-08-27 13:37:56 -0400

    add change text font size variable

--- day3_Scoped_CSS_Variables&JS/vanilla.js
+++ day3_Scoped_CSS_Variables&JS/vanilla.js
@@ -4,6 +4,7 @@
     function changeStyle() {
       const suffix = this.dataset.sizing || '';
       document.documentElement.style.setProperty(`--${this.name}`, this.value + suffix);
+      console.log(this);
     }
 
     inputs.forEach(input => input.addEventListener('change', changeStyle));
